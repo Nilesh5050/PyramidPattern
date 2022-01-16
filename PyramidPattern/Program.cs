a6fd92ce03b16df8a2bebe36730592721479e66e
Nilesh Vishwakarma <iamnilesh2020@gmail.com>
2022-01-16 19:08:35 +0530

    Add project files.

---
+++ PyramidPattern/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PyramidPattern
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 20; i++)
+            {
+                for (int j = i; j <= 20; j++)
+                {
+                    Console.Write(" ");
+
+
+                }
+                for (int k = 1; k <= i; k++)
+                {
+                    Console.Write("*");
+
+                }
+                for (int l = 2; l <= i; l++)
+                {
+                    Console.Write("*");
+
+                }
+                Console.WriteLine();
+                
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
